590f385cefabc766d49d44964359496dc54d1710
Pikin <pikin@bk.ru>
2024-05-06 15:45:00 +0500

    main and questions fragments added

--- app/src/main/java/com/example/hw6/MainActivity.kt
+++ app/src/main/java/com/example/hw6/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.hw6
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import com.example.hw6.databinding.ActivityMainBinding
 
 
@@ -11,8 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+//        val binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
+        supportFragmentManager.commit{
+            replace<Fmain>(R.id.main_container)
+            addToBackStack(Fmain::class.java.simpleName)
+        }
+//            .beginTransaction().replace(R.id.test_frame, Fmain.newInstance())
+//            .commit()
     }
 }
