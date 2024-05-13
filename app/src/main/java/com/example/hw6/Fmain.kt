003d51cdcb048fe2f7378d90179eec397d098a68
Pikin <pikin@bk.ru>
2024-05-13 12:30:31 +0500

    backstack fixed

--- app/src/main/java/com/example/hw6/Fmain.kt
+++ app/src/main/java/com/example/hw6/Fmain.kt
@@ -13,22 +13,15 @@ import com.example.hw6.databinding.FragmentFmainBinding
 
 class Fmain : Fragment() {
 
-
     private var _binding: FragmentFmainBinding? = null
     private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-      }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentFmainBinding.inflate(inflater)
-
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -42,7 +35,4 @@ class Fmain : Fragment() {
         super.onDestroyView()
         _binding = null
     }
-    
-
-
 }
--- app/src/main/java/com/example/hw6/Fquestions.kt
+++ app/src/main/java/com/example/hw6/Fquestions.kt
@@ -5,7 +5,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.addCallback
 import androidx.fragment.app.FragmentManager
 import androidx.navigation.fragment.findNavController
 import com.example.hw6.databinding.FragmentFquestionsBinding
@@ -14,52 +13,31 @@ import com.example.hw6.databinding.FragmentFquestionsBinding
 
 class Fquestions : Fragment() {
 
-
-
     private var _binding: FragmentFquestionsBinding? = null
     private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentFquestionsBinding.inflate(inflater)
-
-
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
-            parentFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
-            findNavController().navigate(R.id.action_fquestions_to_fmain)
-            isEnabled = false
-        }
-
-
         binding.qBackButton.setOnClickListener {
             parentFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
             findNavController().navigate(R.id.action_fquestions_to_fmain)
-
-
         }
+
         binding.qSendButton.setOnClickListener {
             val result = getAnswersByUser()
-
             val action = FquestionsDirections.actionFquestionsToFsend(result)
             findNavController().navigate(action)
-
-
         }
     }
-
 
     override fun onDestroyView() {
         super.onDestroyView()
@@ -68,11 +46,13 @@ class Fquestions : Fragment() {
 
     private fun getAnswersByUser(): String {
         var correctAnswersCount = 0
+        val yourResult = resources.getText(R.string.your_result)
+        val div3 = resources.getText(R.string.div3)
 
         if (binding.question1.checkedRadioButtonId == binding.answer12.id) correctAnswersCount++
         if (binding.question2.checkedRadioButtonId == binding.answer21.id) correctAnswersCount++
         if (binding.question3.checkedRadioButtonId == binding.answer31.id) correctAnswersCount++
 
-        return "Ваш результат: $correctAnswersCount/3"
+        return "$yourResult $correctAnswersCount$div3"
     }
 }
